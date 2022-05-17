c64bd0408c92b8c5ec7443e54c3b7ed609fae5af
Omar <omar.assed@gmail.com>
2022-05-17 10:16:43 -0600

    commit tree 42fea790faa0205f7470c38c5849627bd5f250a1

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,11 +1,11 @@
 describe('index.js', function () {
   describe('scuberGreetingForFeet()', function () {
-    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () { //ride <= 400;//
       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
     });
 
-    it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
-      expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
+    it('charges 20 dollars for a distance between 400 and 2000 feet', function () { //if ride > 400 && ride <= 2000 {charge = 30};//
+      expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.'); 
     });
 
     it('charges 30 dollars for a distance over 2000 feet', function () {
